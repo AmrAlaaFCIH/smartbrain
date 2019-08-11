fb3e75e184fcda778285399d9948b8693b68cb0c
Amr Alaa <46508070+AmrAlaaFCIH@users.noreply.github.com>
2019-08-11 11:23:45 +0200

    SmartBrain App

---
+++ precache-manifest.268f861fb3c36188781c87b29b6b0821.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bebb4e1ba34f352d9faa69b4a7d873e0",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "de60cfbeace2d8c8d29e",
+    "url": "/smartbrain/static/css/main.6f85647f.chunk.css"
+  },
+  {
+    "revision": "60fad08cb58c27e6b052",
+    "url": "/smartbrain/static/js/2.9a83c709.chunk.js"
+  },
+  {
+    "revision": "de60cfbeace2d8c8d29e",
+    "url": "/smartbrain/static/js/main.51948e20.chunk.js"
+  },
+  {
+    "revision": "d58dce6c3cff9d933cca",
+    "url": "/smartbrain/static/js/runtime~main.44d0c8fa.js"
+  }
+]);
